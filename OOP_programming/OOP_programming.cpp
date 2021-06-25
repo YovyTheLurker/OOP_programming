44287043d6c56fbbaf14126e757fe67b77de5c6a
yovyTheLurker <yoerodri@yahoo.com>
2021-06-25 12:39:12 -0400

    Commit all

--- OOP_programming/OOP_programming.cpp
+++ OOP_programming/OOP_programming.cpp
@@ -1,5 +1,5 @@
 // OOP_programming.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Constructors=have the same name as your type and don't have a return type.
 
 #include <iostream>
 #include<list>
@@ -7,28 +7,87 @@ using namespace std;
 //class is a user defined datatype.
 class cars {
 public://access modifier to make these attributes accessible outside of this function.
-	string make;
-	string color;
-	int miles=0;
-	list<string>specs;
+	string Make;
+	string Color;
+	int Miles;
+	list<string>Specifications;
 
+	//constructor
+	cars(string make, string color) {
+		Make = make;
+		Color = color;
+		Miles = 0;
+	};
+
+	void getInfo() {
+		cout << "Make: " << Make << endl;
+		cout << "Color: " << Color << endl;
+		cout << "Miles: " << Miles << endl;
+		cout << "Specs: " << endl;
+		for (string specs : Specifications) {
+			cout << specs << endl;
+		}
+
+	};
+	void milesCount() {
+		Miles++;
+	};
+	void milesCheat() {
+		if (Miles > 0)
+			Miles--;
+	};
+	void specFunc(string specs) {
+		Specifications.push_back(specs);
+	};
 };
 
 int main()
 {
-	cars sedan;
-	sedan.make = "Honda Civic";
-	sedan.color = "white";
-	sedan.miles = 23;
-	sedan.specs = { "2 doors","sunroof","air cond"};
+	cars sedan("Honda Civic","white");
+	sedan.Specifications.push_back("2 doors");
+	sedan.Specifications.push_back("sunroof");
+	sedan.Specifications.push_back("air cond");
+	sedan.milesCount();
+	sedan.getInfo();
 
-	cout << "Make: " << sedan.make << endl;
-	cout << "Color: " << sedan.color << endl;
-	cout << "Miles: " << sedan.miles << endl;
-	cout << "Specs: " << endl;
-	for (string specs : sedan.specs) {
-		cout << specs << endl;
-}
+	cars sedan2("Toyota Corolla", "Black");
+	sedan2.Specifications.push_back("4 doors");
+	sedan2.Specifications.push_back("GPS nav");
+	sedan2.Specifications.push_back("break assist");
+	sedan2.milesCount();
+	sedan2.milesCount();
+	sedan2.milesCount();
+	sedan2.milesCount();
+	sedan2.milesCheat();
+	sedan2.getInfo();
+
+//*****************************************************************************************************************
+	//repeated code no longer needed commented out.
+
+	//sedan.make = "Honda Civic";
+	//sedan.color = "white";
+	//sedan.miles = 23;
+	//sedan.specs = { "2 doors","sunroof","air cond"};
+
+	//cout << "Make: " << sedan.Make << endl;
+	//cout << "Color: " << sedan.Color << endl;
+	//cout << "Miles: " << sedan.Miles << endl;
+	//cout << "Specs: " << endl;
+	//for (string specs : sedan.specs) {
+	//	cout << specs << endl;
+
+	//sedan2.make= "Toyota Corolla";
+	//sedan2.color = "Black";
+	//sedan2.miles = 44;
+	//sedan2.specs = { "4 doors","cruise control","GPS nav" };
+
+	//cout << "Make: " << sedan2.Make << endl;
+	//cout << "Color: " << sedan2.Color << endl;
+	//cout << "Miles: " << sedan2.Miles << endl;
+	//cout << "Specs: " << endl;
+	//for (string specs : sedan2.specs) {
+	//	cout << specs << endl;
+	
 system("pause>0"); 
 }
 
---
+++ Source1.cpp
@@ -0,0 +1,74 @@
+// OOP_programming.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Constructors=have the same name as your type and don't have a return type.
+
+#include <iostream>
+#include<list>
+using namespace std;
+//class is a user defined datatype.
+class cars {
+public://access modifier to make these attributes accessible outside of this function.
+	string Make;
+	string Color;
+	int Miles = 0;
+	list<string>specs;
+
+	//constructor
+	cars(string make, string color) {
+		Make = make;
+		Color = color;
+		Miles = 0;
+	}
+
+	void getInfo() {
+		cout << "Make: " << Make << endl;
+		cout << "Color: " << Color << endl;
+		cout << "Miles: " << Miles << endl;
+		cout << "Specs: " << endl;
+		for (string specs : specs) {
+			cout << specs << endl;
+		}
+
+	};
+
+};
+
+int main()
+{
+	cars sedan("Honda Civic", "white");
+	sedan.specs.push_back("2 doors");
+	sedan.specs.push_back("sunroof");
+	sedan.specs.push_back("air cond");
+	cars sedan2("Toyota Corolla", "Black");
+
+	sedan.getInfo();
+	sedan2.getInfo();
+
+	//*****************************************************************************************************************
+		//repeated code no longer needed commented out.
+
+		//sedan.make = "Honda Civic";
+		//sedan.color = "white";
+		//sedan.miles = 23;
+		//sedan.specs = { "2 doors","sunroof","air cond"};
+
+		//cout << "Make: " << sedan.Make << endl;
+		//cout << "Color: " << sedan.Color << endl;
+		//cout << "Miles: " << sedan.Miles << endl;
+		//cout << "Specs: " << endl;
+		//for (string specs : sedan.specs) {
+		//	cout << specs << endl;
+
+		//sedan2.make= "Toyota Corolla";
+		//sedan2.color = "Black";
+		//sedan2.miles = 44;
+		//sedan2.specs = { "4 doors","cruise control","GPS nav" };
+
+		//cout << "Make: " << sedan2.Make << endl;
+		//cout << "Color: " << sedan2.Color << endl;
+		//cout << "Miles: " << sedan2.Miles << endl;
+		//cout << "Specs: " << endl;
+		//for (string specs : sedan2.specs) {
+		//	cout << specs << endl;
+
+	system("pause>0");
+}
